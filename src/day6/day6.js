978206479c2f48494dc63bf4444cc7f09c9e51f8
Mirasii <meatheat_100mean@hotmail.co.uk>
2022-12-07 18:39:37 +0000

    use JSON to parametise results

--- src/day6/day6.js
+++ src/day6/day6.js
@@ -1,15 +1,17 @@
+const parts = require('./day6.json');
+
 function day6(input) {
     var index = 0;
-    var resultObj = {
-        partOne: null,
-        partTwo: null
-    };
-    while (resultObj.partOne == null || resultObj.partTwo == null) {
-        if (resultObj.partOne == null) {
-            resultObj.partOne = checkSubstring(input, index, 4, 'data');
-        }
-        if (resultObj.partTwo == null) {
-            resultObj.partTwo = checkSubstring(input, index, 14, 'message');
+    var resultObj = {};
+    var keys = Object.keys(parts);
+    for (var key of keys) {
+        resultObj[key] = null;
+    }
+    while (Object.values(resultObj).some(element => element === null)) {
+        for (var key of keys) {
+            if (resultObj[key] === null) {
+                resultObj[key] = checkSubstring(input, index, parts[key].indicator, parts[key].type);
+            }
         }
         index++;
         if (index == input.length) break;
