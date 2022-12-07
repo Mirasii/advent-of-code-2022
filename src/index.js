060a08a848908f84a4c354ea6b6ef9dbbd508a9a
Mirasii <meatheat_100mean@hotmail.co.uk>
2022-12-07 18:25:27 +0000

    add day 6 to index

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 const { readFileSync } = require("fs");
 const { day1 } = require("./day1/day1.js");
+const { day6 } = require("./day6/day6.js");
 
 const daysToRun = [
-  day1,
-  // add additional days here
+    day1,
+    day6
+    // add additional days here
 ];
 
 daysToRun.forEach((day) => {
-  const name = day.name;
-  const input = readFileSync(`./src/inputs/${name}`).toString();
-  console.log(`${name}:`, day(input));
+    const name = day.name;
+    const input = readFileSync(`./src/inputs/${name}`).toString();
+    console.log(`${name}:`, day(input));
 });
