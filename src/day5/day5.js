7698d4cc500ee287282de284c8d8987bdc22ded6
Mirasii <meatheat_100mean@hotmail.co.uk>
2022-12-07 21:56:24 +0000

    day5

---
+++ src/day5/day5.js
@@ -0,0 +1,65 @@
+function day5(input) {
+    const rawData = input.replaceAll('\r', '').split("\n\n");
+    const groupByFour = (array) => {
+        const result = [];
+        for (let i = 0; i < array.length; i += 4) {
+            result.push(array.slice(i, i + 4));
+        }
+        return result;
+    };
+
+    var crateMatrix = rawData[0].split("\n");
+    crateMatrix.pop();
+
+    var crateMatrix = crateMatrix
+        .map((a) => a.split(""))
+        .map((a) => groupByFour(a))
+        .map((a) => a.map((b) => b[1]))
+        .reverse();
+    crateMatrix = crateMatrix[0].map((_, colIndex) => crateMatrix.map((row) => row[colIndex]));
+    crateMatrix = crateMatrix.map((a) => a.filter((b) => b !== " "));
+    const moveRegex = /move (\d+) from (\d+) to (\d+)/;
+    const moves = rawData[1]
+        .trim()
+        .split("\n")
+        .map((a) => moveRegex.exec(a))
+        .map((a) => [a[1], a[2], a[3]].map(Number));
+
+    var originalMatrix = JSON.stringify(crateMatrix);
+    //part1
+    for (var move of moves) {
+        var from = crateMatrix[move[1] - 1];
+        var to = crateMatrix[move[2] - 1];
+        for (var i = 1; i <= move[0]; i++) {
+            to.push(from[from.length - 1]);
+            from.pop();
+        }
+    }
+    var topOfStacks1 = crateMatrix.reduce((topVals, crateArray) => topVals + crateArray[crateArray.length - 1], '');
+    //reset matrix
+    crateMatrix = JSON.parse(originalMatrix);
+    //part2
+    for (var move of moves) {
+        var from = crateMatrix[move[1] - 1];
+        var to = crateMatrix[move[2] - 1];
+        var moving = Array.from((move[0] == 1 ? from[from.length - 1] : from.slice(from.length - move[0], from.length)));
+
+        crateMatrix[move[2] - 1] = to.concat(moving)
+        for (var i = 1; i <= move[0]; i++) {
+            from.pop();
+        }
+    }
+    var topOfStacks2 = crateMatrix.reduce((topVals, crateArray) => topVals + crateArray[crateArray.length - 1], '');
+
+    return {
+        "partOne": topOfStacks1,
+        "partTwo": topOfStacks2
+    }
+}
+
+
+
+
+module.exports = {
+    day5,
+};
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const { day1 } = require("./day1/day1.js");
 const { day2 } = require("./day2/day2.js");
 const { day3 } = require("./day3/day3.js");
 const { day4 } = require("./day4/day4.js");
+const { day5 } = require("./day5/day5.js");
 const { day6 } = require("./day6/day6.js");
 
 const daysToRun = [
@@ -10,6 +11,7 @@ const daysToRun = [
     day2,
     day3,
     day4,
+    day5,
     day6
     // add additional days here
 ];
